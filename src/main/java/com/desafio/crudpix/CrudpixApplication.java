326372c5efa6525aa75988f671401ecf92285df3
Pedrinhsp <pedrinho.top07@gmail.com>
2024-06-07 19:02:10 -0300

    Commit Inicial

---
+++ src/main/java/com/desafio/crudpix/CrudpixApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.crudpix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudpixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudpixApplication.class, args);
+	}
+
+}
